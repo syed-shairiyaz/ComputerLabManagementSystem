005322e05e6d39767d2d12e6c4fb2f00e205e68e
syed-shairiyaz <syedshairiyaz23@gmail.com>
2026-08-09 23:19:27 +0530

    replacing new config file

--- config.py
+++ config.py
@@ -2,9 +2,18 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+DATABASE_DIR = os.path.join(BASE_DIR, "database")
+
+# Make sure the database folder exists
+os.makedirs(DATABASE_DIR, exist_ok=True)
+
+
 class Config:
+
     SECRET_KEY = "clms_super_secret_key_2026"
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database", "clms.db")
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + os.path.join(DATABASE_DIR, "clms.db")
+    )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
