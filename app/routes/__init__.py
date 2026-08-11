46142ce2820e90408d5f9a87f943a01d0e7ebd10
syed-shairiyaz <syedshairiyaz23@gmail.com>
2026-08-11 21:49:20 +0530

    registering blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,3 +3,4 @@ from .student_routes import student_bp
 from .teacher_routes import teacher_bp
 from .attendance_routes import attendance_bp
 from .computer_routes import computer_bp
+from .announcement_routes import announcement_bp
